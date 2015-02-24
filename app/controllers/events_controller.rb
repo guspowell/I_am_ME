7175da3db7b0c138221120130a5e3afa4c21145d
Danielle Demkiw <ddemkiw@gmail.com>
2015-02-24 13:26:07 +0000

    removed even params require

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -54,6 +54,6 @@ class EventsController < ApplicationController
     end
 
     def event_params
-      params.require(:event).permit(:title, :date, :description)
+      params.permit(:title, :date, :description)
     end
 end
