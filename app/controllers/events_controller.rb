def132c1b67a72bad89899184f42a8de300bf87c
ciawalsh <cia.walsh@gmail.com>
2015-02-25 12:24:41 +0000

    Paperclip uploading images to AWS, and events showing images

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,7 +14,7 @@ class EventsController < ApplicationController
 
   # GET /events/new
   def new
-    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: 201, acl: :public_read)
+    # @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: 201, acl: :public_read)
     @event = Event.new
   end
 
