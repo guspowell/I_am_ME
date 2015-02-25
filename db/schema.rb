9a73e6ae48883df994884e38f87d89a2cdbd20af
India Dearlove <india_dearlove@yahoo.co.uk>
2015-02-25 20:15:45 +0000

    Merge

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 20150225175148) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.string   "image_url"
     t.integer  "user_id"
     t.integer  "calendar_id"
     t.string   "location"
