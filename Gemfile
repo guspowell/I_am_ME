ca63a63a443d58180044c4ca774f9b13ca9bbfe4
ciawalsh <cia.walsh@gmail.com>
2015-02-25 12:42:05 +0000

    Added Users & Sign Up

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'aws-sdk', '< 2.0'
 gem "paperclip", "~> 4.2"
+gem 'devise'
 
 group :development, :test do
   gem 'byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :events
+
+  # root to: "events#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
