1b62840aad9c4b30d91604226192b788ed25dede
guspowell <augustuspowell@me.com>
2015-02-25 15:53:15 +0000

    test for json passing

--- app/views/events/show.json.jbuilder
+++ app/views/events/show.json.jbuilder
@@ -2,4 +2,3 @@
   json.extract! @event, :id, :name, :date, :description, :created_at, :updated_at
 
   json.image @event.image, :url
-
--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Events", type: :request do
-  
+
   describe "GET /events" do
 
     it "returns event as json" do
       event = create(:event)
-      get "/events/#{event.id}"
+      get "/events/#{event.id}.json"
       expect_json({name: "Football"})
-    end  
+    end
   end
 
-  describe "POST /events" do 
+  describe "POST /events" do
 
     it "creates an event" do
-      post "/events/", :event => {name: "Rollerderby"}
+      post "/events.json", :event => {name: "Rollerderby"}
       expect(response).to be_success
       expect(Event.find_by_name("Rollerderby")).not_to be_nil
-    end 
-  end 
+    end
+  end
 
 
 end
