9f779fd75390f758728b57c61cc417c98f13547b
Danielle Demkiw <ddemkiw@gmail.com>
2015-02-26 17:30:09 +0000

    translating javascript to erb

--- app/assets/javascripts/event.js
+++ app/assets/javascripts/event.js
@@ -1,43 +1,43 @@
-console.log("Hi Demkiw!");
+// console.log("Hi Demkiw!");
 
-var numberOfhours = 24;
-var numberOfDays = 7; 
+// var numberOfhours = 24;
+// var numberOfDays = 7; 
 
-$(document).ready(function(){
-  buildCalendar();
-  buildWidgetThumbnail();
-});
+// $(document).ready(function(){
+//   buildCalendar();
+//   buildWidgetThumbnail();
+// });
 
-var buildHourUnit = function(hour, day){
-  var hourDiv = document.createElement('div');
-  return $(hourDiv).attr('class', 'hours').attr('id', "hour" + hour);
-};
+// var buildHourUnit = function(hour, day){
+//   var hourDiv = document.createElement('div');
+//   return $(hourDiv).attr('class', 'hours').attr('id', "hour" + hour);
+// };
 
-var buildDayUnit = function(day) {
-  var daySection = document.createElement('section');
-  return $(daySection).attr('class', "day").attr('id', "day" + day);
-};
+// var buildDayUnit = function(day) {
+//   var daySection = document.createElement('section');
+//   return $(daySection).attr('class', "day").attr('id', "day" + day);
+// };
 
-var buildCalendar = function(){
-  var days  = 1;
-  var hours;
+// var buildCalendar = function(){
+//   var days  = 1;
+//   var hours;
   
-  while(days <= numberOfDays){
-    var hours = 1;
-    var day = buildDayUnit(days).appendTo($('div#calendar'));
+//   while(days <= numberOfDays){
+//     var hours = 1;
+//     var day = buildDayUnit(days).appendTo($('div#calendar'));
 
-    while(hours <= numberOfhours){
-      buildHourUnit(hours, days).appendTo(day);
-      hours++;
-    }
-    days++;
-  }
-};
+//     while(hours <= numberOfhours){
+//       buildHourUnit(hours, days).appendTo(day);
+//       hours++;
+//     }
+//     days++;
+//   }
+// };
 
-var buildWidgetThumbnail = function(){ 
-  var thumbnailDiv =  $.parseHTML("<div class='thumbnail'> <h1> flerp derp </h1><a href='#'></a></div>")
-  $(thumbnailDiv).appendTo($('div#hour1.hours'))
-};
+// var buildWidgetThumbnail = function(){ 
+//   var thumbnailDiv =  $.parseHTML("<div class='thumbnail'> <h1> flerp derp </h1><a href='#'></a></div>")
+//   $(thumbnailDiv).appendTo($('div#hour1.hours'))
+// };
 
 // var buildFrameUnit = function(frameNumber){
 //   var frameDiv = document.createElement('div');
