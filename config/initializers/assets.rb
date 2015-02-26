406ede8f92edbca309020307f4ae45cfeb812cf3
India Dearlove <india_dearlove@yahoo.co.uk>
2015-02-26 17:48:54 +0000

    MAP DONE

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,4 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( event_widget.css )
 Rails.application.config.assets.precompile += %w( normalize.css )
+Rails.application.config.assets.precompile += %w( map.css )
