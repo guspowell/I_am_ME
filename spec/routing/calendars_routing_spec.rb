47729610e0413f3aa7ad31015f680d637e80d4e5
Danielle Demkiw <ddemkiw@gmail.com>
2015-02-26 15:42:29 +0000

    passing routes tests for calendar

--- spec/routing/calendars_routing_spec.rb
+++ spec/routing/calendars_routing_spec.rb
@@ -1,34 +1,41 @@
 require "rails_helper"
 
 RSpec.describe CalendarsController, type: :routing do
+  
+
   describe "routing" do
 
+    before do
+       user     = create(:user)
+       calendar = create(:calendar, user: user)
+    end
+
     it "routes to #index" do
-      expect(:get => "/calendars").to route_to("calendars#index")
+      expect(:get => "//users/:user_id/calendars").to route_to({"controller"=>"calendars", "action"=>"index", "user_id"=>":user_id"})
     end
 
     it "routes to #new" do
-      expect(:get => "/calendars/new").to route_to("calendars#new")
+      expect(:get => "/users/:user_id/calendars/new").to route_to({"controller"=>"calendars", "action"=>"new", "user_id"=>":user_id"})
     end
 
     it "routes to #show" do
-      expect(:get => "/calendars/1").to route_to("calendars#show", :id => "1")
+      expect(:get => "/users/:user_id/calendars/:id").to route_to({"controller"=>"calendars", "action"=>"show", "user_id"=>":user_id", "id"=>":id"})
     end
 
     it "routes to #edit" do
-      expect(:get => "/calendars/1/edit").to route_to("calendars#edit", :id => "1")
+      expect(:get => "/users/:user_id/events/:id/edit").to route_to({"controller"=>"events", "action"=>"edit", "user_id"=>":user_id", "id"=>":id"})
     end
 
     it "routes to #create" do
-      expect(:post => "/calendars").to route_to("calendars#create")
+      expect(:post => "/users/:user_id/calendars").to route_to({"controller"=>"calendars", "action"=>"create", "user_id"=>":user_id"})
     end
 
     it "routes to #update" do
-      expect(:put => "/calendars/1").to route_to("calendars#update", :id => "1")
+      expect(:put => "/users/:user_id/calendars/1").to route_to({"controller"=>"calendars", "action"=>"update", "user_id"=>":user_id", "id"=>"1"})
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/calendars/1").to route_to("calendars#destroy", :id => "1")
+      expect(:delete => "/users/:user_id/calendars/1").to route_to({"controller"=>"calendars", "action"=>"destroy", "user_id"=>":user_id", "id"=>"1"})
     end
 
   end
