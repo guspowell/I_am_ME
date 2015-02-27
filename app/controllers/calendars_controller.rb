1d8ef35c3476d025a7e7ef14e5afa3c50271fa89
Ptolemy <ptolemybarnes@gmail.com>
2015-02-27 18:39:55 +0000

    Solve residual issues with merge conflict.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,4 +1,4 @@
-class CalendarsController < ApplicationControlle
+class CalendarsController < ApplicationController
   before_action :set_calendar, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
