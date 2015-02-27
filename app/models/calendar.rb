5786b2d975702226bb4dcb29b23d2c908a44b29d
Danielle Demkiw <ddemkiw@gmail.com>
2015-02-27 10:31:10 +0000

    fixing event index

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -4,4 +4,13 @@ class Calendar < ActiveRecord::Base
   has_many :events
   belongs_to :user, dependent: :destroy
 
+  def find_date
+    @today = Time.now
+    @day = @time.to_date
+    until @day.monday? 
+      @day = @day.prev_day
+    end 
+    p @day.strftime('%Y-%m-%d')
+  end
+
 end
