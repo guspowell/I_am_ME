af7c6d972a01fa8ac0e98de68747b85341fd08e6
guspowell <augustuspowell@me.com>
2015-02-28 16:34:34 +0000

    styled map

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -1,16 +1,99 @@
 $(document).ready(function() {
 
   function initialize() {
-    
+
     var mapOptions = {
       center: { lat: 51.517369, lng: -0.073602},
       zoom: 15
     };
 
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    
+	var styles = [
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#444444"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    }
+  ]
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  map.setOptions({styles: styles});
+
   }
 
   google.maps.event.addDomListener(window, 'load', initialize);
+
 
 });
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,4 @@
+class PagesController < ApplicationController
+  def map
+  end
+end
