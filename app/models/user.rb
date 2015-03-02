4abfddafd40dc21dbc42e9b53a6122527bd90586
ciawalsh <cia.walsh@gmail.com>
2015-03-02 11:36:44 +0000

    Added Username to DB and now can login using Usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,28 @@ class User < ActiveRecord::Base
   has_many :calendars
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => [:login]
+
+  attr_accessor :login
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end
+
+  validates :username,
+    :presence => true,
+    :uniqueness => {
+      :case_sensitive => false
+    }
+
 end
