661c3dd161c615585721729818f83786e38ca0fc
Danielle Demkiw <ddemkiw@gmail.com>
2015-03-02 18:51:01 +0000

    testing auto me_cal function

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,9 @@ RSpec.describe User, type: :model do
   context 'upon registration' do 
 
     it 'should automatically create a me calendar' do
-      user = create(:user)
-      expect(user.calendars.length).to eq(1)
+      expect(Calendar.all.length).to eq(0)
+      create(:user)
+      expect(Calendar.all.length).to eq(1)
     end 
 
   end
