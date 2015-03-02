8a77fb57b305052a438c6a8039aa838d1f574f06
Ptolemy <ptolemybarnes@gmail.com>
2015-03-02 20:14:47 +0000

    Fix problem with missing helper method

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,7 +10,6 @@ class Event < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
-
   def add_tag(tag)
     self.tags << tag 
   end
@@ -19,5 +18,8 @@ class Event < ActiveRecord::Base
     self.tags
   end
 
+  def get_tags_as_strings
+    self.tags.map {|tag| tag.name }
+  end
 
 end
