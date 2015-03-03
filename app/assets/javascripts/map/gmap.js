41253ecba37e7e7bc88e2d7602b1196db6589b58
India Dearlove <india_dearlove@yahoo.co.uk>
2015-03-03 15:52:53 +0000

    CHange widget size and google map changes

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -6,12 +6,21 @@ $(document).ready(function() {
       var widget = $(this).closest("[class^='widget']").find('#map-canvas').get(0);
       $(widget).show();
       var des = $(this).closest("[class^='widget']").find('.description').get(0);
-      $(des).show();
+      $(des).hide();
+      var play = $(this).closest("[class^='widget']").find('.play').get(0);
+      $(play).hide();
+      var more = $(this).closest("[class^='widget']").find('.more').get(0);
+      $(more).hide();
+      var title = $(this).closest("[class^='widget']").find('.title').get(0);
+      $(title).hide();
       
       if($(this).hasClass('hide')){
         console.log("hide")
         $(widget).hide();
-        $(des).hide();
+        $(des).show();
+        $(play).show();
+        $(more).show();
+        $(title).show();
         $(this).addClass('show').removeClass('hide');
 
       }
