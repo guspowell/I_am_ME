fe24a8e79318e5ddea4a9597a1bffc4a10a43c78
Ptolemy <ptolemybarnes@gmail.com>
2015-03-03 13:50:32 +0000

    Update spec/helpers with helper for filling in form.

--- spec/features/events_feature_spec.rb
+++ spec/features/events_feature_spec.rb
@@ -9,18 +9,15 @@ feature 'Event' do
       create(:calendar, user: user)
       login_as(user)
       visit "/users/#{user.id}/events/new"
-      
-      fill_form(:event, {  name: 'Superhero Dance Off', 'event_date_1i' => '2015',
-                          'event_date_2i'=>'March', 'event_date_3i'=>'2',
-                          'event_date_4i'=>'12',
-                          'event_description'=>'Why is this happening?',
-                          'event_location'=>   'Hyde Park' })
+
+      attrs = attributes_for(:event)
+      fill_in_event_form(attrs)
 
 
       attach_file('Image', "#{Rails.root}/spec/support/uploads/spiderman.jpg")
       click_button 'Create Event'
 
-      expect(page).to have_content('Superhero Dance Off')
+      expect(page).to have_content(attrs[:name])
     end
   end
 
@@ -40,5 +37,6 @@ feature 'Event' do
     end 
   end
 end
+
 
 
