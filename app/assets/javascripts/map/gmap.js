8cdf26747afd8880a91a804fcc6357fad0b35883
India Dearlove <india_dearlove@yahoo.co.uk>
2015-03-03 12:39:49 +0000

    All maps working

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -2,105 +2,21 @@ $(document).ready(function() {
 
   function initialize() {
 
-    var mapOptions = {
-      center: { lat: 51.517369, lng: -0.073602},
-      zoom: 15
-    };
+    $('.map').click(function() {
+      var widget = $(this).closest("[class^='widget']").find('#map-canvas').get(0);
 
-  	var styles = [
-      {
-          "featureType": "administrative",
-          "elementType": "labels.text.fill",
-          "stylers": [
-              {
-                  "color": "#444444"
-              }
-          ]
-      },
-      {
-          "featureType": "landscape",
-          "elementType": "all",
-          "stylers": [
-              {
-                  "color": "#f2f2f2"
-              }
-          ]
-      },
-      {
-          "featureType": "poi",
-          "elementType": "all",
-          "stylers": [
-              {
-                  "visibility": "off"
-              }
-          ]
-      },
-      {
-          "featureType": "road",
-          "elementType": "all",
-          "stylers": [
-              {
-                  "saturation": -100
-              },
-              {
-                  "lightness": 45
-              }
-          ]
-      },
-      {
-          "featureType": "road.highway",
-          "elementType": "all",
-          "stylers": [
-              {
-                  "visibility": "simplified"
-              }
-          ]
-      },
-      {
-          "featureType": "road.arterial",
-          "elementType": "labels.icon",
-          "stylers": [
-              {
-                  "visibility": "off"
-              }
-          ]
-      },
-      {
-          "featureType": "transit",
-          "elementType": "all",
-          "stylers": [
-              {
-                  "visibility": "off"
-              }
-          ]
-      },
-      {
-          "featureType": "water",
-          "elementType": "all",
-          "stylers": [
-              {
-                  "color": "#000000"
-              },
-              {
-                  "visibility": "on"
-              }
-          ]
-      }
-    ]
+    
+      var mapOptions = {
+        center: { lat: 51.517369, lng: -0.073602},
+        zoom: 15
+      };
 
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    map.setOptions({styles: styles});
-
-    var myLatlng = new google.maps.LatLng(51.517369,-0.073602);
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title: 'Hello World!'
+      var map = new google.maps.Map(widget, mapOptions);
+    
     });
 
   }
 
   google.maps.event.addDomListener(window, 'load', initialize);
-
 
 });
