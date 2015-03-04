fb29c95362450bf302da2f47f25a77fcc38b86fa
India Dearlove <india_dearlove@yahoo.co.uk>
2015-03-04 14:29:20 +0000

    Set up css to change on event page

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -11,11 +11,17 @@ $(document).ready(function() {
     var widget = $(this).closest("[class^='widget']");
     var widgetClass;
 
-    if (widget.attr('class') === 'widget-small') {
+    if (widget.attr('class') === 'widget-small' ) {
       widgetClass = enlargeWidget(widget);
+    }
+    if (widget.attr('class') === 'widget-page' ) {
+      widgetClass = enlargeWidgetPage(widget);
     }
     if (widget.attr('class') === 'widget-large') {
       widgetClass = shrinkWidget(widget);
+    }
+    if (widget.attr('class') === 'widget-large-page') {
+      widgetClass = shrinkWidgetPage(widget);
     }
 
     widget.attr('class', widgetClass);
@@ -40,6 +46,27 @@ $(document).ready(function() {
 
     return 'widget-small';
   }
+
+  var enlargeWidgetPage = function(widget) {
+    var scopedWidgetWidth  = $(widget).width();
+    var scopedWidgetHeight = $(widget).height();
+
+    widget.animate({height: scopedWidgetHeight*3 + 'px'}, 250);
+    widget.animate({width: scopedWidgetWidth*1.65 + 'px'}, 250);
+
+    return  'widget-large-page';
+  }
+
+  var shrinkWidgetPage = function(widget) {
+    var scopedWidgetWidth  = $(widget).width();
+    var scopedWidgetHeight = $(widget).height();
+
+    widget.animate({height: scopedWidgetHeight/3 + 'px'}, 250);
+    widget.animate({width: scopedWidgetWidth/1.65 + 'px'}, 250);
+
+    return 'widget-page';
+  }
+
 
 
 });
