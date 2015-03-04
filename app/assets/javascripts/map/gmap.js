1ac572782b956625bdf2c97ed16b3ddfb1b42fb8
ciawalsh <cia.walsh@gmail.com>
2015-03-04 11:35:58 +0000

    before rails migration for geocoding

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -40,32 +40,32 @@ $(document).ready(function() {
         //   });
         // };
 
-        var geocoding = new google.maps.Geocoder();
-        $("#submit_button_geocoding").click(function(){
-          codeAddress(geocoding);
-        });
-        $("#submit_button_reverse").click(function{
-          codeLatLng(geocoding);
-        });
+        // var geocoding = new google.maps.Geocoder();
+        // $("#submit_button_geocoding").click(function(){
+        //   codeAddress(geocoding);
+        // });
+        // $("#submit_button_reverse").click(function{
+        //   codeLatLng(geocoding);
+        // });
 
-        function codeAddress(geocoding){
-          var address = $("#search_box_geocoding").val();
-          if(address.length > 0){
-            geocoding.geocode({'address': address}, function(results, status){
-              if(status == google.maps.GeocoderStatus.OK){
-                map.setCenter(results[0].geometry.location);
-                var marker = new google.maps.Marker({
-                  map: map,
-                  position: results[0].geometry.location
-                });
-              } else {
-                alert("Geocode was not successful for the following reason: " + status);
-              }
-            });
-          } else {
-            alert("Search field can't be blank");
-          }
-        }
+        // function codeAddress(geocoding){
+        //   var address = $("#search_box_geocoding").val();
+        //   if(address.length > 0){
+        //     geocoding.geocode({'address': address}, function(results, status){
+        //       if(status == google.maps.GeocoderStatus.OK){
+        //         map.setCenter(results[0].geometry.location);
+        //         var marker = new google.maps.Marker({
+        //           map: map,
+        //           position: results[0].geometry.location
+        //         });
+        //       } else {
+        //         alert("Geocode was not successful for the following reason: " + status);
+        //       }
+        //     });
+        //   } else {
+        //     alert("Search field can't be blank");
+        //   }
+        // }
         
         var map = new google.maps.Map(widget, mapOptions);
         $(this).addClass('hide').removeClass('show');
