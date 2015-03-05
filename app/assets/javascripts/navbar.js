8dc955d4078bd15438d642922b24cc04858dd977
Ptolemy <ptolemybarnes@gmail.com>
2015-03-05 18:42:01 +0000

    Pass cal feature test: user can click outside the create event box to return to cal view.

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,17 +1,38 @@
 $(document).ready(function() {
 
-  $('.add-cal').click(function() {
-    $('#background-blur').show();
-  });
+  var createCalBox = $('#background-blur');
 
+  var showAddCal = function() {
+    createCalBox.show();
+    $('.add-cal').unbind();
+    createCalBox.click(clickAway);
+  }
+
+  var hideAddCal = function(callback) {
+    createCalBox.unbind();
+    createCalBox.hide(2, bindAddCalClick);
+  }
+
+  var clickAway = function(e) {
+    var container = $('#new-cal')
+    
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      hideAddCal(bindAddCalClick);
+    }
+  }
+
+  var bindAddCalClick = function() {
+    $('.add-cal').click(showAddCal)
+  }
+
+  bindAddCalClick();
+     
   $('.plus').click(function() {
     var createEventBox = $('#background-create-event');
     createEventBox.show();
-    
 
     createEventBox.click(function (e)  {
       var container = $('form#new_event');
-      console.log(container);
 
       if (!container.is(e.target) && container.has(e.target).length === 0) {
         createEventBox.hide();
