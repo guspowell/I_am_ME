ebb0e0196708b32b7d1ac2983987194885d2dfc1
Danielle Demkiw <ddemkiw@gmail.com>
2015-03-05 13:54:32 +0000

    adding css correction and test for tags

--- spec/features/tag_feature_spec.rb
+++ spec/features/tag_feature_spec.rb
@@ -1,9 +1,22 @@
 require 'rails_helper'
+require 'formulaic'
 
 feature 'Tags' do
 
   scenario "adding tags to events" do
+    user= create(:user)
     
+    login_as(user)
+
+    visit user_calendar_path(user, user.calendars.me)
+    
+    attrs = attributes_for(:event)
+    fill_in_event_form(attrs)
+    fill_in 'Tags', with: 'Sports'
+    click_button 'submit'
+
+    expect(Event.find_by(name: attrs[:name]).tags.first.name).to eq('Sports')
+
 
   end
 
